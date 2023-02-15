3516c29794b06ea3d00806d31b3a038c17f002d6
ben.dang <ben.dang@geocomply.com>
2023-02-15 12:09:03 +0700

    init project

---
+++ SampleCoreProject/SampleCoreProject/SampleCoreProject.swift
@@ -0,0 +1,11 @@
+//
+//  SampleCoreProject.swift
+//  SampleCoreProject
+//
+//  Created by Tri.Dang on 15/02/2023.
+//
+
+public class SampleCoreProject {
+    public var sampleVariable: String = "SampleCoreProject"
+    public init() { }
+}
